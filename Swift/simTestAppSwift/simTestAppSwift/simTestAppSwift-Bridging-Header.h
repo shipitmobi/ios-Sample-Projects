0b7cb4a113f6fc4bd70bbe823b961543ac159a92
gaurav2k1 <gaurav.jaimini@gmail.com>
2017-02-12 03:26:18 +0530

    [SIM] : Update Swift Sample App.
    Signed-off-by: gaurav2k1 <gaurav.jaimini@gmail.com>

---
+++ Swift/simTestAppSwift/simTestAppSwift/simTestAppSwift-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <shipitmobi/shipitmobi.h>
+#import <shipitmobi/SIM_AlertView.h>
+#import <shipitmobi/SIM_InAppArtist.h>
+#import <shipitmobi/SIM_NotificationArtist.h>
+#import <shipitmobi/SIM_RegMon.h>
+#import <shipitmobi/SIM_StarRating.h>
